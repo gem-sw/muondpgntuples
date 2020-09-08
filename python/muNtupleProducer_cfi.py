8dfbb3c934cd8f1b33acef5ee80ce95d3b26d04d
Eliza Melo Da Costa <eliza.melo.da.costa@cern.ch>
2020-09-08 12:24:14 +0000

    Upload New File

---
+++ python/muNtupleProducer_cfi.py
@@ -0,0 +1,22 @@
+import FWCore.ParameterSet.Config as cms
+
+muNtupleProducer = cms.EDAnalyzer("MuNtupleProducer",
+                                  storeRpcRecHits = cms.untracked.bool(True),
+                                  rpcRecHitLabel = cms.untracked.InputTag("rpcRecHits"),
+                                  ph1DtDigiTag = cms.untracked.InputTag("muonDTDigis"),
+                                  ph2DtDigiTag = cms.untracked.InputTag("none"),
+
+                                  ph1DtSegmentTag = cms.untracked.InputTag("dt4DSegments"),        
+                                  ph2DtSegmentTag = cms.untracked.InputTag("none"),
+
+                                  ph1DTtTrigMode = cms.untracked.string('DTTTrigSyncFromDB'),
+                                  ph1DTtTrigModeConfig = cms.untracked.PSet(vPropWire = cms.double(24.4),
+                                                                            doTOFCorrection = cms.bool(False),
+                                                                            tofCorrType = cms.int32(2),
+                                                                            wirePropCorrType = cms.int32(0),
+                                                                            doWirePropCorrection = cms.bool(False),
+                                                                            doT0Correction = cms.bool(True),
+                                                                            tTrigLabel = cms.string(''),
+                                                                            debug = cms.untracked.bool(False)
+                                                                        )
+        )
